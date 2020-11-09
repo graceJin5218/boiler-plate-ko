378fe61608057878d178838bcfbb8f75a0e113f5
yeonjin <kyj881214@gmail.com>
2020-11-09 13:09:13 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const app =express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yeonjin:9057coco@boilerplate.rtmn5.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
+app.get('/', (req, res) => res.send('Hello world'))
 
+app.post('/register',(req, res) => {
 
-app.get('/', (req, res) => res.send('Hello'))
+    //회원가입 할 때 필요한 정보들을 Client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err,userInfo) => {
+        if(err) return res.json({success: false, err})
+
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port,() => console.log('Example on port ${port}'))
